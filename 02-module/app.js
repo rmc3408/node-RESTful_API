ad5bf08213818a65c3435b8b9ca1d211a6a03ba3
rmc3408 <rmc3408@protonmail.com>
2021-01-10 17:15:48 -0500

    section2 done

--- 02-module/app.js
+++ 02-module/app.js
@@ -1,10 +1,66 @@
 //console.log(global);
 //console.log(module);
 
-const log = require('./logger');
-log('printing by function');
-
-//const logger = require('./logger');
-//logger.myLog('print by object created');
 
 
+//WHEN export only object
+//const mylog = require('./logger');
+//myLog('printing by function');
+const logger = require('./logger');
+logger.myLog('print by object created');
+
+
+
+//Module PATH
+//log(module);
+const path = require('path');
+const pathFile = path.parse(__filename);
+console.log(pathFile);
+
+
+
+//MODULE OS
+const os = require('os');
+const total = os.totalmem();
+console.log('Total of ' + total + ' bytes of memory');
+console.log('Free ' + os.freemem() + ' bytes of memory');
+
+
+//MODULE File System
+const fs = require('fs');
+const whereSyn = fs.readdirSync('./');
+console.log(whereSyn);
+
+const whereAsync = fs.readdir('./', (err, files) => { // asyncronous execute after all syncronous.
+console.log('Error is ', err);
+console.log('Result is ', files);
+});
+
+
+
+//Module EVENT - import a class called Emitter
+const EventEmmiter = require('events'); // assign to clas
+const myEmission = new EventEmmiter(); // create object type EventClassEmmiter
+//register a listener
+myEmission.on('shine the light', function (myEvent) {
+    console.log('Message caught by listener and ', myEvent);
+} );
+myEmission.emit('shine the light', { id: 1, LogFile:logger.endPoint, url:'http://'});
+
+
+
+
+// Module HTTP
+const site = require('http');
+const mySite = site.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('Hello People');
+        res.end();
+    }
+    if (req.url === '/api') {
+        res.write(JSON.stringify(logger.endPoint));
+        res.end();
+    }
+});
+mySite.listen(3000); 
+console.log('listening on port 3000');
--- 02-module/logger.js
+++ 02-module/logger.js
@@ -1,12 +1,14 @@
-var x =;
 var url = 'http://mylogger.io/log';
 
-function log(msg) {
+function myLog(msg) {
     console.log(msg);
+    
 }
 
 //module.exports.myLog = log;
 //console.log(module);
 module.exports.endPoint = url;
-module.exports = log;
+module.exports.myLog = myLog;
+
+//module.exports = myLog;
 
