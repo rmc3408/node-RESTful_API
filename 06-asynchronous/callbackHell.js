4c66c2d9c14189534e203ca86e0cc21f6793d25b
rmc3408 <rmc3408@protonmail.com>
2021-01-19 12:19:20 -0500

    callback HELL

---
+++ 06-asynchronous/callbackHell.js
@@ -0,0 +1,38 @@
+getUser(1, () => {
+  getRepos(user.gitName, (reps) => {
+      getCommits(repo, (com) => {
+          console.log(com);
+    });
+  });
+});
+
+getUser(1, getRepositories);
+
+function getRepositories(user) {
+    getRepositories(user.gitName, getCommits);
+}
+function getCommits(reps) {
+    getCommits(repo, displayCommits);
+}
+function displayCommits(com) {
+    console.log(com);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getUser(id, callback) {
+  setTimeout(() => {
+    console.log("Reading user from database...");
+    callback({ id: id, gitName: "rmc3408" }); //it is like return for Asyncronous
+  }, 2000);
+}
