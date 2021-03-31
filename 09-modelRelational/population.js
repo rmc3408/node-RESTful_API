9e4bcda6987950bee0aeeaab81d938f8268fd9f3
rmc3408 <rmc3408@protonmail.com>
2021-03-31 14:41:15 -0400

    video 2 done

--- 09-modelRelational/population.js
+++ 09-modelRelational/population.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/playground')
+mongoose.connect('mongodb://localhost/playground', { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
@@ -12,6 +12,11 @@ const Author = mongoose.model('Author', new mongoose.Schema({
 
 const Course = mongoose.model('Course', new mongoose.Schema({
   name: String,
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Author'
+  }
+
 }));
 
 async function createAuthor(name, bio, website) { 
@@ -20,7 +25,6 @@ async function createAuthor(name, bio, website) {
     bio, 
     website 
   });
-
   const result = await author.save();
   console.log(result);
 }
@@ -38,12 +42,13 @@ async function createCourse(name, author) {
 async function listCourses() { 
   const courses = await Course
     .find()
-    .select('name');
+    .populate('author', '-_id name')
+    .select('name author');
   console.log(courses);
 }
 
-createAuthor('Mosh', 'My bio', 'My Website');
+//createAuthor('Mosh', 'My bio', 'My Website');
 
-// createCourse('Node Course', 'authorId')
+//createCourse('Node Course', '6064beccb99a610470699bda');
 
-// listCourses();
+listCourses();
